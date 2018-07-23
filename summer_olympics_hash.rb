7fa7ab918d35df1a8c3ee2ef236ca77e49b81ead
Corey Garcia <corey.w.garcia@outlook.com>
2018-07-23 22:43:33 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,10 +1,10 @@
 
 def create_olympics_hash
   hash = {}
-  hash[Sydney] = 2000
-  hash[Athens] = 2004
-  hash[Beijing] = 2008
-  hash[London] = 2012
+  hash["Sydney"] = 2000
+  hash["Athens"] = 2004
+  hash["Beijing"] = 2008
+  hash["London"] = 2012
   return hash
 end
 
